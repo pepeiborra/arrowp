4d4c7d8a264a04c524d57ec3433d5009458c7ea3
pepeiborra <pepeiborra@gmail.com>
2017-06-11 09:22:37 +0000

    Replace local State type with transformers package

--- preprocessor/State.hs
+++ preprocessor/State.hs
@@ -2,14 +2,5 @@
 
 module State(State, runState, get, put) where
 
-newtype State s a = State { runState :: s -> (a, s) }
+import Control.Monad.Trans.State
 
-instance Monad (State s) where
-	return x = State (\n -> (x, n))
-	State v >>= f = State (\n -> let (x, n') = v n in runState (f x) n')
-
-get :: State s s
-get = State (\s -> (s, s))
-
-put :: s -> State s ()
-put s = State (\_ -> ((), s))
