0383bd9425544f26d1c5d0e387422a03d7954208
Pepe Iborra <pepeiborra@gmail.com>
2017-08-20 15:30:38 +0200

    Replace SYB with Uniplate

--- src/Control/Arrow/Notation.hs
+++ src/Control/Arrow/Notation.hs
@@ -9,19 +9,15 @@ module Control.Arrow.Notation
   ) where
 
 import           Data.Generics.Uniplate.Data
-import           Debug.Hoed.Pure             (observe)
 import           Language.Haskell.Exts       as H hiding (Tuple)
 
 import           ArrSyn
 import           Utils
 
 translateModule :: Module SrcSpanInfo -> Module SrcSpanInfo
-translateModule = observe "translateModule" (everywhere (mkT translateExp))
-
+translateModule = transformBi translateExp
 
 translateExp :: Exp SrcSpanInfo -> Exp SrcSpanInfo
-translateExp = translateExp'
-translateExp' :: Exp SrcSpanInfo -> Exp SrcSpanInfo
-translateExp' (Proc _ pat exp) =
-  fmap getSrcSpanInfo $ ArrSyn.translate (fmap S pat) (fmap S exp)
-translateExp' other            = other
+translateExp (Proc _ pat exp) =
+  getSrcSpanInfo <$> ArrSyn.translate (fmap S pat) (fmap S exp)
+translateExp other = other
