3a373d143a0093f53a8f65bf52603db591571b0f
Pepe Iborra <pepeiborra@gmail.com>
2017-08-24 14:07:35 +0200

    Add another, more applicable static cond. example

--- examples/Static.hs
+++ examples/Static.hs
@@ -9,14 +9,15 @@ ifEx outputResultsArg processor outputSink = proc inputs -> do
   if outputResultsArg
     then outputSink -< results
     else returnA -< ()
-  returnA -< results
+  processor -< results
 
 ifEx' :: Arrow a => Bool -> a inp out -> a out () -> a inp out
-ifEx' outputResultsArg processor outputSink
-  = ((processor >>> arr (\ results -> (results, results))) >>>
-       (first
-          (if outputResultsArg then outputSink else arr (\ results -> ()))
-          >>> arr (\ (_, results) -> results)))
+ifEx' outputResultsArg processor outputSink = proc inputs -> do
+  results <- processor -< inputs
+  results <- if outputResultsArg
+    then outputSink -< results
+    else returnA -< results
+  processor -< results
 
 caseEx :: Arrow a => Bool -> a inp out -> a out () -> a inp out
 caseEx outputResultsArg processor outputSink = proc inputs -> do
@@ -24,4 +25,12 @@ caseEx outputResultsArg processor outputSink = proc inputs -> do
   case outputResultsArg of
     True -> outputSink -< results
     False -> returnA -< ()
-  returnA -< results
+  processor -< results
+
+caseEx' :: Arrow a => Bool -> a inp out -> a out () -> a inp out
+caseEx' outputResultsArg processor outputSink = proc inputs -> do
+  results <- processor -< inputs
+  results <- case outputResultsArg of
+               True -> outputSink -< results
+               False -> returnA -< results
+  processor -< results
